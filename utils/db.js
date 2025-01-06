9ecdbdd684c46ca456f37c0ed485691ad00e700d
BenRyan0 <benryanrinconada5@gmail.com>
2025-01-07 01:34:03 +0800

    2nd commit

--- utils/db.js
+++ utils/db.js
@@ -4,10 +4,10 @@ require("dotenv").config();
 module.exports.dbConnect = async () => {
   try {
     if(process.env.mode === 'pro'){
-      await mongoose.connect(process.env.DB_LOCAL, {});
+      await mongoose.connect(process.env.DB_URL, {});
       console.log("production database connected ...");
    }else{
-    await mongoose.connect(process.env.DB_URL, {});
+    await mongoose.connect(process.env.DB_LOCAL, {});
     console.log("local database connected ...");
    }
   } catch (error) {
